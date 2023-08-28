8a47a54a760e4366fdc762b09ce666d0216814b6
Rishop Babu <rishopbabu22@gmail.com>
2023-08-28 18:33:45 +0530

    StatusBar component

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import SelectionListComponent from './Components/SelectionListComponent';
+import StatusBarComponent from './Components/StatusBarComponent';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => SelectionListComponent);
+AppRegistry.registerComponent(appName, () => StatusBarComponent);
