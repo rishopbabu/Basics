269cfcfb9aa3346c3ecf417a0fd09cefa6e40894
Rishop Babu <rishopbabu22@gmail.com>
2023-08-28 18:27:58 +0530

    Page refresh component

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import PressableComponent from './Components/PressableComponent';
+import PageRefreshConponent from './Components/PageRefreshComponent';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => PressableComponent);
+AppRegistry.registerComponent(appName, () => PageRefreshConponent);
