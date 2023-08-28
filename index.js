cadf3495cc5867694c47935078b90f8936419072
Rishop Babu <rishopbabu22@gmail.com>
2023-08-28 18:30:31 +0530

    selectionListComponent

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import PageRefreshConponent from './Components/PageRefreshComponent';
+import SelectionListComponent from './Components/SelectionListComponent';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => PageRefreshConponent);
+AppRegistry.registerComponent(appName, () => SelectionListComponent);
