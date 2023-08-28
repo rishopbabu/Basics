6d889cd03c79e752c9d95a29f03f6f9d4efdac2a
Rishop Babu <rishopbabu22@gmail.com>
2023-08-28 18:36:58 +0530

    toggle switch component

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import StatusBarComponent from './Components/StatusBarComponent';
+import ToggleSwitchComponent from './Components/ToggleSwitchComponent';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => StatusBarComponent);
+AppRegistry.registerComponent(appName, () => ToggleSwitchComponent);
