1ef428af901fa575ece36dcab802fd2feab5ca54
Rishop Babu <rishopbabu22@gmail.com>
2023-08-28 15:31:39 +0530

    state implemented

--- App.tsx
+++ App.tsx
@@ -5,8 +5,8 @@
  * @format
  */
 
-import React from 'react';
-import { SafeAreaView, ScrollView, Text, Image, View, TextInput } from 'react-native';
+import React, {useState} from 'react';
+import { SafeAreaView, ScrollView, Text, Image, View, TextInput, TouchableOpacity, useWindowDimensions } from 'react-native';
 
 type AppProps = {
   name: String;
@@ -23,7 +23,37 @@ const AppName = (props: AppProps) => {
 
 const ImageView = () => {
   return(
-      <Image source={require('./assets/emoji1.png')} style= {{width: 100, height: 100}}></Image>
+      <Image source={require('./assets/emoji1.png')} style= {{width: 100, height: 100}}/>
+  )
+}
+
+
+// State - state is like a component’s personal data storage. 
+// State is useful for handling data that changes over time or that comes from user interaction.
+const ImageChanger = () => {
+  const [imageSource, setImageSource] = useState(require('./assets/emoji1.png'))
+  const [isImage1, setIsImage1] = useState(true)
+
+  const handleButtonClick = () => {
+    setImageSource(require('./assets/emoji2.png'))
+  }
+
+  const toggeImage = () => {
+    if (isImage1) {
+      setImageSource(require('./assets/emoji1.png'))
+    } else {
+      setImageSource(require('./assets/emoji2.png'))
+    }
+    setIsImage1(!isImage1)
+  }
+
+  return(
+    <View>
+     <TouchableOpacity onPress={toggeImage}>
+      <Text>Change Image</Text>
+     </TouchableOpacity>
+     <Image source={imageSource} style= {{width: 100, height: 100}}></Image>
+    </View>
   )
 }
 
@@ -38,7 +68,7 @@ const App = () => {
           <AppName name={'Second RN'}/>
           <AppName name={'Third RN'}/>
           <Image source={require('./assets/emoji1.png')} style= {{width: 100, height: 100}}></Image>
-          <ImageView/>
+          <ImageChanger/>
         </View>
         <TextInput style= {{height: 40, borderBlockColor: 'gray', borderWidth: 0.5}} placeholder='Type here'></TextInput>
       </ScrollView>
