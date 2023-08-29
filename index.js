e177ecbda266ee7efa976e2d25a10c11dd8d39b0
Rishop Babu <rishopbabu22@gmail.com>
2023-08-29 06:37:14 +0530

    viewstyleprops completed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import DisplayAnImageWithStyle from './Props/ImageStyleProps';
+import ViewStyleProps from './Props/ViewStyleProps';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => DisplayAnImageWithStyle);
+AppRegistry.registerComponent(appName, () => ViewStyleProps);
