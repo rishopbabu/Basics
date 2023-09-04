608e94980798489c83e5cf97daf2fcd2305149a7
Rishop Babu <rishopbabu22@gmail.com>
2023-09-04 09:20:14 +0530

    pan responder

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import ScrollViewAnimation from './Animation/ScrollViewWithAnimated';
+import PanResponderAnimation from './Animation/PanResponder';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => ScrollViewAnimation);
+AppRegistry.registerComponent(appName, () => PanResponderAnimation);
